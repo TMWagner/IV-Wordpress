937d030b3bfac6b78ba43b2c86fe4394f2f97b74
Thomas Wagner <twagner@ivlab.com>
2014-03-24 11:38:57 -0700

    cleanup accidental directory move

--- wp-content/themes/twentyeleven/thematic-emod-child/sidebar-aboutleft.php
+++
@@ -1,6 +0,0 @@
-<?php if ( is_active_sidebar( 'aboutleft' ) ) : ?>
-	<div id="sidebar-aboutleft" class="sidebar">
-		<?php dynamic_sidebar( 'aboutleft' ); ?>
-	</div>
-<?php endif; ?>
-	
--- wp-content/themes/twentyeleven/thematic-emod-child/sidebar-team.php
+++
@@ -1,6 +0,0 @@
-<?php if ( is_active_sidebar( 'team' ) ) : ?>
-	<div id="sidebar-team" class="sidebar">
-		<?php dynamic_sidebar( 'team' ); ?>
-	</div>
-<?php endif; ?>
-	
